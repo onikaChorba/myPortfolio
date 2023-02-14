aaef8ca8175ff678b339bbb62a20dc2748fe2540
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2023-02-14 21:22:29 +0200

    create structure in site, add header and home, add button and style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Header } from "./components/Header/Header";
+import "./index.scss";
+import { FrontPage } from "./Pages/FrontPage/FrontPage";
 function App() {
-  return <div className="App">Hi</div>;
+  return (
+    <div className="wrapper">
+      <Header />
+      <menu className="mainContainer">
+        <FrontPage />
+      </menu>
+    </div>
+  );
 }
 
 export default App;
