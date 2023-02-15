fa1a7ebc1c09e8da3974b5175d6897b7cff18f96
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2023-02-15 20:50:55 +0200

    add section about, add icon

--- src/Pages/FrontPage/sections/Home/Home.jsx
+++ src/Pages/FrontPage/sections/Home/Home.jsx
@@ -6,8 +6,8 @@ export const Home = () => {
       <h1 className="home__title1">
         Hi, I`m <span>Onika Chorba</span>
       </h1>
-      <h2 className="home__title2"> Frontend developer</h2>
-      <p className="home__text">
+      <h2 className="home__title2 title2"> Frontend developer</h2>
+      <p className="home__text text">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore nisi
         quidem, delectus repellat repellendus neque recusandae exercitationem ad
         veniam nemo, obcaecati nihil, totam laudantium perspiciatis doloribus
