790d2ab103f477440730ad68f203bca139b32c01
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2023-02-21 10:33:42 +0200

    add style in resume section, add contact form and style contact form

---
+++ src/Pages/FrontPage/sections/Contact/Contact.jsx
@@ -0,0 +1,14 @@
+import { ContactForm } from "../../components/ContactForm/ContactForm";
+
+export const Contact = () => {
+  return (
+    <section className="contact section">
+      <h2 className="contact__title title2">Contact</h2>
+      <p className="text">
+        Feel free to Contact me by submitting the form below and I will get back
+        to you as soon as possible
+      </p>
+      <ContactForm />
+    </section>
+  );
+};
